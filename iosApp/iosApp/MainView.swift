ace164d6dcf0fc59a4f2a4c19e0968bd098db832
Bahadır Seyfi <57216650+bahadirseyfi@users.noreply.github.com>
2024-04-14 05:01:28 +0200

    Update-mainViewComponent-iOS

--- iosApp/iosApp/MainView.swift
+++ iosApp/iosApp/MainView.swift
@@ -11,12 +11,12 @@ import Shared
 import MapKit
 
 struct MainView: View {
-    @ObservedObject private(set) var viewModel: MainViewModel
+    @EnvironmentObject var viewModel: MainViewModel
     
     var body: some View {
-        VStack {
+        ZStack {
             updateIU()
-        }
+        }.ignoresSafeArea()
         .onAppear {
             viewModel.getChargePoints()
         }
@@ -28,23 +28,62 @@ struct MainView: View {
             return AnyView(Text("Loading"))
             
         case .result(let resultArray):
-            return AnyView(mapContent(locations: resultArray))
+            return AnyView(viewContent(locations: resultArray))
             
         case .error(let result):
             return AnyView(Text("\(result)"))
         }
     }
     
-    private func mapContent(locations: [PoiModel]) -> some View {
+    private func viewContent(locations: [PoiModel]) -> some View {
         ZStack {
-            Map(coordinateRegion: $viewModel.mapRegion,
-                annotationItems: viewModel.locations,
-                annotationContent: { location in
-                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: (location.addressInfo?.latitude).orZero,
-                                                                 longitude: (location.addressInfo?.longitude).orZero)) {
-
-                }
-            }).preferredColorScheme(.dark)
+            mapContent(locations: locations)
+            VStack {
+               // TODO: - Header
+                
+                Spacer()
+                
+                stationInfoContent(locations: locations)
+            }
+           
         }
+    }
+    
+    private func mapContent(locations: [PoiModel]) -> some View {
+        Map(coordinateRegion: $viewModel.mapRegion,
+            annotationItems: locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: (location.addressInfo?.latitude).orZero,
+                                                             longitude: (location.addressInfo?.longitude).orZero)) {
+                MapAnnotationView()
+                    .scaleEffect(viewModel.mapLocation.id == location.id ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        viewModel.selectedLocation(location: location)
+                    }
+            }
+        }).preferredColorScheme(.dark)
+    }
+    
+    private func stationInfoContent(locations: [PoiModel]) -> some View {
+        ZStack {
+            ForEach($viewModel.locations, id: \.id) { location in
+                if (viewModel.mapLocation.id).orZero == (location.id) {
+                    StationInfoView(location: location,
+                                    nextButtonAction: viewModel.nextButtonAction)
+                        .shadow(color: Color.black.opacity(0.2), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(insertion: .move(edge: .trailing),
+                                                removal: .move(edge: .leading)))
+                        .overlay(alignment: .topTrailing) {
+                            Button {
+                              //
+                            } label: {
+                              //
+                            }
+                        }
+                }
+            }
+        }.animation(.bouncy, value: viewModel.mapLocation.id)
     }
 }
