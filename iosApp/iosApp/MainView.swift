7af6dbb23aba178658cb4ec7341cfec3d66bd38a
Bahadır Seyfi <57216650+bahadirseyfi@users.noreply.github.com>
2024-04-14 04:18:54 +0200

    Add-mapTView-iOS

--- iosApp/iosApp/MainView.swift
+++ iosApp/iosApp/MainView.swift
@@ -7,17 +7,44 @@
 //
 
 import SwiftUI
+import Shared
+import MapKit
 
 struct MainView: View {
     @ObservedObject private(set) var viewModel: MainViewModel
     
     var body: some View {
         VStack {
-            ForEach(viewModel.points, id: \.id) { item in
-                Text(item.uuid ?? "")
-            }
-        }.onAppear {
+            updateIU()
+        }
+        .onAppear {
             viewModel.getChargePoints()
+        }
+    }
+    
+    func updateIU() -> AnyView {
+        switch viewModel.launches{
+        case .loading:
+            return AnyView(Text("Loading"))
+            
+        case .result(let resultArray):
+            return AnyView(mapContent(locations: resultArray))
+            
+        case .error(let result):
+            return AnyView(Text("\(result)"))
+        }
+    }
+    
+    private func mapContent(locations: [PoiModel]) -> some View {
+        ZStack {
+            Map(coordinateRegion: $viewModel.mapRegion,
+                annotationItems: viewModel.locations,
+                annotationContent: { location in
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: (location.addressInfo?.latitude).orZero,
+                                                                 longitude: (location.addressInfo?.longitude).orZero)) {
+
+                }
+            }).preferredColorScheme(.dark)
         }
     }
 }
