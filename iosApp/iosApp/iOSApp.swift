bf521923dafaa1df1313e7253a07928bbf3cc55e
Bahadır Seyfi <57216650+bahadirseyfi@users.noreply.github.com>
2024-04-14 04:19:15 +0200

    Configure-appType-iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -6,7 +6,7 @@ struct iOSApp: App {
 	var body: some Scene {
         let sdk = ChargePointsRepository()
 		WindowGroup {
-            MainView(viewModel: MainView.MainViewModel(kmmRepository: sdk))
+            MainView(viewModel: MainViewModel(kmmRepository: sdk))
 		}
 	}
 }
