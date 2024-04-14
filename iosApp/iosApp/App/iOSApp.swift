8caf5b0721ded7204c15bb883fc5cc33668ca9dc
Bahadır Seyfi <57216650+bahadirseyfi@users.noreply.github.com>
2024-04-14 22:38:50 +0200

    Update-injectionType-iOS

--- iosApp/iosApp/App/iOSApp.swift
+++ iosApp/iosApp/App/iOSApp.swift
@@ -1,14 +1,23 @@
 import SwiftUI
 import Shared
+import SwiftData
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-        let sdk = ChargePointsRepository()
-        @StateObject var viewModel = MainViewModel(kmmRepository: sdk)
-		WindowGroup {
-            MainView()
-                .environmentObject(viewModel)
-		}
-	}
+    var body: some Scene {
+        let kmmRepository = ChargePointsRepository()
+        
+        WindowGroup {
+            TabView {
+                MainView(kmmRepository: kmmRepository)
+                    .tabItem {
+                        Label("Main", systemImage: "gear")
+                    }
+                FavoriteView()
+                    .tabItem {
+                        Label("Favorite", systemImage: "gear")
+                    }
+            }
+        }
+    }
 }
