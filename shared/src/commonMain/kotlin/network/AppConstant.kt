26a83004f20f7509f6076b0e884b832450f5ab72
Bahadır Seyfi <57216650+bahadirseyfi@users.noreply.github.com>
2024-04-15 01:03:04 +0200

    Update-appConstants-KMM

--- shared/src/commonMain/kotlin/network/AppConstant.kt
+++ shared/src/commonMain/kotlin/network/AppConstant.kt
@@ -3,4 +3,6 @@ package network
 object AppConstant {
     const val BASE_URL = "https://api.openchargemap.io/v3/poi"
     const val API_KEY = "b555f612-2f9e-4976-8bbf-9f9b68287842"
+    const val PAGING = "maxresults" // TODO: - Manipulate for pagination
+    const val TYPING = "camelcase"
 }
